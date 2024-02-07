a077e1557a7c749f0bb6bc1475d423f734d4d8b5
minyoung0503 <155047307+minyoung0503@users.noreply.github.com>
2024-02-07 14:26:24 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers, n) {
+    var answer = 0;
+    for (i of numbers) {
+        answer += i;
+        if (answer > n) {
+            break;
+        }
+    }
+    return answer;
+}
