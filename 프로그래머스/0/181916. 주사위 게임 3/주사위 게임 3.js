bb091b0b4bd4ef9a47cff5c24e2ba6d0dc5a93b3
minyoung0503 <155047307+minyoung0503@users.noreply.github.com>
2024-03-21 13:53:40 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -0,0 +1,28 @@
+function solution(a, b, c, d) {
+    let arr = new Array(6).fill(0);
+    let diceArr = [a, b, c, d];
+    let p = 0;
+    let q = 0;
+    let r = 0;
+    for (num of diceArr) {
+        arr[num - 1] += 1;
+    }
+    
+    if (arr.indexOf(4) !== -1) {
+        return 1111 * a;
+    } else if (arr.indexOf(3) !== -1) {
+        p = arr.indexOf(3) + 1;
+        q = arr.indexOf(1) + 1;
+        return (10 * p + q) ** 2;
+    } else if (arr.indexOf(2) !== arr.lastIndexOf(2) && arr.indexOf(1) === -1) {
+        p = arr.indexOf(2) + 1;
+        q = arr.lastIndexOf(2) + 1;
+        return (p + q) * Math.abs(p - q);
+    } else if (arr.indexOf(2) !== -1 && arr.indexOf(2) === arr.lastIndexOf(2) && arr.indexOf(1) !== -1) {
+        q = arr.indexOf(1) + 1;
+        r = arr.lastIndexOf(1) + 1;
+        return q * r;
+    } else {
+        return arr.indexOf(1) + 1;
+    }
+}
